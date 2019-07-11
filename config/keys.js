82576960845905e6e1489b5dad00b849698cca0b
Pascal Nsunba <nsumbapascal@gmail.com>
2019-07-11 04:27:33 +0200

    added .env.example file

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI: `mongodb+srv://pascal:${process.env.PASSWORD}@cluster0-qrpd8.mongodb.net/test?retryWrites=true&w=majority`
+    MongoURI: `mongodb+srv://pascal:${process.env.PASSWORD}@cluster0-qrpd8.mongodb.net/profile_of_the_user_app?retryWrites=true&w=majority`
 }
